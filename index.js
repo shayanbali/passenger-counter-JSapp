eb54ba32b5fa40c0435e90f73b3c14e6a289e34d
shayan <shayanbali@gmail.com>
2022-08-09 19:49:47 +0430

    Increament button

---
+++ index.js
@@ -0,0 +1,8 @@
+// document.getElementById("count-el").innerText = 6;
+
+let count = 0
+function increament() {
+    console.log(count)
+    count++
+    document.getElementById("count-el").innerText = count
+}
